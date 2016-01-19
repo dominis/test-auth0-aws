4c1a4c3bc9fd4f527bfa2482628c4c64e92c3372
Nandor Sivok <dominis@haxor.hu>
2016-01-19 19:23:51 +0100

    node: added autoscaling plolicy based on a cloudwatch metric

--- terraform/node.tf
+++ terraform/node.tf
@@ -1,43 +1,10 @@
-resource "aws_elb" "jobtest" {
-  name = "jobtest-elb"
-
-  security_groups = ["${aws_security_group.nat.id}"]
-  subnets = ["${aws_subnet.us-west-1a-public.id}", "${aws_subnet.us-west-1c-public.id}"]
-  listener {
-    instance_port = 80
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
-  }
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    target = "HTTP:80/"
-    interval = 5
-  }
-
-  cross_zone_load_balancing = true
-  idle_timeout = 400
-  connection_draining = true
-  connection_draining_timeout = 400
-
-  tags {
-      Name = "auth0-jobtest"
-      Environment = "dev"
-      Owner = "dominis"
-      Role = "node"
-  }
-}
-
-
 resource "aws_launch_configuration" "launch_config" {
   name = "jobtest-lc"
   image_id = "${var.node_ami_id}"
   instance_type = "${var.node_instance_type}"
   key_name = "${var.aws_key_name}"
   security_groups = ["${aws_security_group.nat.id}"]
+  key_name = "${var.aws_key_name}"
   #user_data = "${file(var.user_data)}"
 
 }
@@ -57,9 +24,8 @@ resource "aws_autoscaling_group" "main_asg" {
   desired_capacity = "${var.asg_number_of_instances}"
 
   health_check_grace_period = "10"
-  health_check_type = "ELB"
+  health_check_type = "EC2"
 
-  load_balancers = ["${aws_elb.jobtest.name}"]
   tag {
     key = "Name"
     value = "auth0-jobtest-node-ASG"
@@ -82,4 +48,26 @@ resource "aws_autoscaling_group" "main_asg" {
   }
 }
 
+resource "aws_autoscaling_policy" "node" {
+    name = "jobtest-asg-policy"
+    scaling_adjustment = 2
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.main_asg.name}"
+}
 
+resource "aws_cloudwatch_metric_alarm" "node" {
+    alarm_name = "jobtest-watch"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "CPUUtilization"
+    namespace = "AWS/EC2"
+    period = "60"
+    statistic = "Average"
+    threshold = "70"
+    dimensions {
+        AutoScalingGroupName = "${aws_autoscaling_group.main_asg.name}"
+    }
+    alarm_description = "app node ec2 cpu utilization"
+    alarm_actions = ["${aws_autoscaling_policy.node.arn}"]
+}
